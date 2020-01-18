11ef56b4517a4733314cba4469da08a4a76642d4
richie-south <richard.soderman@gmail.com>
2020-01-18 12:30:55 +0100

    format: prettier

--- more-flow-app/src/lib/build-block.ts
+++ more-flow-app/src/lib/build-block.ts
@@ -1,7 +1,11 @@
-import { Blocks, Block } from "./block-types"
-import { getLargestXPosition, getSmallestXPosition, getCenterBlocksPosition } from "./get-positions"
+import {Blocks, Block} from './block-types'
+import {
+  getLargestXPosition,
+  getSmallestXPosition,
+  getCenterBlocksPosition
+} from './get-positions'
 
-function buildFirstChildOfMany (
+function buildFirstChildOfMany(
   blocks: Blocks,
   blocksArray: Array<[string, Block]>,
   children: Array<[string, Block]>,
@@ -27,28 +31,23 @@ function buildFirstChildOfMany (
     largestX: number
     blockPath: Array<string>
   }
-) {
+) {
   const xPosition = getCenterBlocksPosition(
     children,
     xOffset,
     parrentBlockX,
     parrentBlockWidth
   )
-  return buildBlock(
-    blocks,
-    blocksArray,
-    childKey,
-    {
-      x: xPosition,
-      y: parrentBlockY + yOffset,
-      height: childHeight,
-      width: childWidth,
-      yOffset,
-      xOffset,
-      largestX,
-      blockPath: [...blockPath, childKey]
-    }
-  )
+  return buildBlock(blocks, blocksArray, childKey, {
+    x: xPosition,
+    y: parrentBlockY + yOffset,
+    height: childHeight,
+    width: childWidth,
+    yOffset,
+    xOffset,
+    largestX,
+    blockPath: [...blockPath, childKey]
+  })
 }
 
 type BuildChildMeta = {
@@ -62,70 +61,67 @@ type BuildChildMeta = {
   blockPath?: Array<string>
 }
 
-export function buildBlock (
-    blocks: Blocks,
-    blocksArray: Array<[string, Block]>,
-    parrentBlockKey: string,
-    {
-      x,
-      y,
-      width,
-      height,
-      yOffset = 150,
-      xOffset = 30,
-      largestX = 0,
-      blockPath = []
-    }: BuildChildMeta
+export function buildBlock(
+  blocks: Blocks,
+  blocksArray: Array<[string, Block]>,
+  parrentBlockKey: string,
+  {
+    x,
+    y,
+    width,
+    height,
+    yOffset = 150,
+    xOffset = 30,
+    largestX = 0,
+    blockPath = []
+  }: BuildChildMeta
 ): Blocks {
   const parrentBlock = {
     ...blocks[parrentBlockKey],
     x,
     y,
     width,
-    height,
+    height
   }
 
-  const children = blocksArray.filter(([_key, _block]) => {
-    return _block.parrents.includes(parrentBlockKey)
-  })
+  const children = blocksArray.filter(([_key, _block]) =>
+    _block.parrents.includes(parrentBlockKey)
+  )
 
   /**
    * split children under parrent
    */
-  const positionedChildren = children.reduce((_children, [childKey, child], index) => {
-    if (index === 0) {
-      const firstChildTree = buildFirstChildOfMany(
-        blocks,
-        blocksArray,
-        children,
-        childKey,
-        {
-          parrentBlockX: parrentBlock.x,
-          parrentBlockY: parrentBlock.y,
-          parrentBlockWidth: parrentBlock.width,
-          childHeight: child.height,
-          childWidth: child.width,
-          blockPath,
-          largestX,
-          xOffset,
-          yOffset
+  const positionedChildren = children.reduce(
+    (_children, [childKey, child], index) => {
+      if (index === 0) {
+        const firstChildTree = buildFirstChildOfMany(
+          blocks,
+          blocksArray,
+          children,
+          childKey,
+          {
+            parrentBlockX: parrentBlock.x,
+            parrentBlockY: parrentBlock.y,
+            parrentBlockWidth: parrentBlock.width,
+            childHeight: child.height,
+            childWidth: child.width,
+            blockPath,
+            largestX,
+            xOffset,
+            yOffset
+          }
+        )
+        return {
+          ..._children,
+          ...firstChildTree
         }
-      )
-      return {
-        ..._children,
-        ...firstChildTree
       }
-    }
 
-    const _childrenArray = Object.entries(_children)
-    const largestXPosition = getLargestXPosition(_childrenArray)
+      const _childrenArray = Object.entries(_children)
+      const largestXPosition = getLargestXPosition(_childrenArray)
 
-    const activeBlocks = buildBlock(
-      blocks,
-      blocksArray,
-      childKey,
-      {
-        x: largestXPosition + (xOffset),
+      const activeBlocks = buildBlock(blocks, blocksArray, childKey, {
+        x: largestXPosition + xOffset,
         y: parrentBlock.y + yOffset,
         height: child.height,
         width: child.width,
@@ -133,27 +129,28 @@ export function buildBlock (
         xOffset,
         largestX: largestXPosition,
         blockPath: [...blockPath, childKey]
+      })
+
+      const repositionedBlocks = repositionBlocks(
+        blocks,
+        blocksArray,
+        childKey,
+        activeBlocks,
+        largestXPosition,
+        xOffset,
+        yOffset,
+        parrentBlock.y + yOffset,
+        child.height,
+        child.width
+      )
+
+      return {
+        ..._children,
+        ...repositionedBlocks
       }
-    )
-
-    const repositionedBlocks = repositionBlocks(
-      blocks,
-      blocksArray,
-      childKey,
-      activeBlocks,
-      largestXPosition,
-      xOffset,
-      yOffset,
-      parrentBlock.y + yOffset,
-      child.height,
-      child.width
-    )
-
-    return {
-      ..._children,
-      ...repositionedBlocks
-    }
-  }, {} as Blocks)
+    },
+    {} as Blocks
+  )
 
   return {
     ...positionedChildren,
@@ -161,7 +158,7 @@ export function buildBlock (
   }
 }
 
-function repositionBlocks (
+function repositionBlocks(
   blocks: Blocks,
   blocksArray: Array<[string, Block]>,
   blockKey: string,
@@ -173,7 +170,6 @@ function repositionBlocks (
   height: number,
   width: number
 ): Blocks {
-
   const activeBlocksArray = Object.entries(activeBlocks)
   if (activeBlocksArray.length > 1) {
     const smallestX = getSmallestXPosition(activeBlocksArray)
@@ -182,28 +178,29 @@ function repositionBlocks (
      * if colliding
      */
     if (smallestX < largestXOfPreviousRootBlocks) {
-      return buildBlock(
-        blocks,
-        blocksArray,
-        blockKey,
-        {
-          // use larger offset when between two block trees
-          x: largestXOfPreviousRootBlocks + (largestXOfPreviousRootBlocks - smallestX) + (xOffset * 3),
-          y: startBlockY,
-          height: height,
-          width: width,
-          yOffset,
-          xOffset,
-          largestX: largestXOfPreviousRootBlocks + (largestXOfPreviousRootBlocks - smallestX) + xOffset,
-        }
-      )
+      return buildBlock(blocks, blocksArray, blockKey, {
+        // use larger offset when between two block trees
+        x:
+          largestXOfPreviousRootBlocks +
+          (largestXOfPreviousRootBlocks - smallestX) +
+          xOffset * 3,
+        y: startBlockY,
+        height: height,
+        width: width,
+        yOffset,
+        xOffset,
+        largestX:
+          largestXOfPreviousRootBlocks +
+          (largestXOfPreviousRootBlocks - smallestX) +
+          xOffset
+      })
     }
   }
 
   return activeBlocks
 }
 
-export function buildBlocks (
+export function buildBlocks(
   blocks: Blocks,
   height: number,
   width: number,
@@ -212,83 +209,78 @@ export function buildBlocks (
 ): Blocks {
   const blocksArray = Object.entries(blocks)
   const startBlocks = blocksArray.filter(
-    ([_, block]) => block.parrents.length === 0)
+    ([_, block]) => block.parrents.length === 0
+  )
 
   const startBlockX = width / 5
   const startBlockY = height / 6
   let previousRootBlocks: Blocks = {}
-  const _blocks = startBlocks.reduce((_blocks, [startBlockKey, startBlock], index) => {
+  const _blocks = startBlocks.reduce(
+    (_blocks, [startBlockKey, startBlock], index) => {
+      if (index === 0) {
+        const _startBlock = {
+          ...startBlock,
+          x: startBlockX,
+          y: startBlockY
+        } as Block
 
-    if (index === 0) {
-      const _startBlock = {
-        ...startBlock,
-        x: startBlockX,
-        y: startBlockY
-      } as Block
-
-      const rootBlocks = {
-        [startBlockKey]: _startBlock,
-        ...buildBlock(
-          blocks,
-          blocksArray,
-          startBlockKey,
-          {
+        const rootBlocks = {
+          [startBlockKey]: _startBlock,
+          ...buildBlock(blocks, blocksArray, startBlockKey, {
             x: startBlockX,
             y: startBlockY,
             height: startBlock.height,
             width: startBlock.width,
             xOffset,
-            yOffset,
-          }
-        )
+            yOffset
+          })
+        }
+
+        previousRootBlocks = rootBlocks
+
+        return {
+          ..._blocks,
+          ...rootBlocks
+        }
       }
 
-      previousRootBlocks = rootBlocks
+      const prevoiusRootBlocksArray = Object.entries(previousRootBlocks)
+      const largestXOfPreviousRootBlocks = getLargestXPosition(
+        prevoiusRootBlocksArray
+      )
 
-      return {
-        ..._blocks,
-        ...rootBlocks
-      }
-    }
-
-    const prevoiusRootBlocksArray = Object.entries(previousRootBlocks)
-    const largestXOfPreviousRootBlocks = getLargestXPosition(prevoiusRootBlocksArray)
-
-    const activeBlocks = buildBlock(
-      blocks,
-      blocksArray,
-      startBlockKey,
-      {
-        x: largestXOfPreviousRootBlocks + (xOffset),
+      const activeBlocks = buildBlock(blocks, blocksArray, startBlockKey, {
+        x: largestXOfPreviousRootBlocks + xOffset,
         y: startBlockY,
         height: startBlock.height,
         width: startBlock.width,
         yOffset,
         xOffset,
-        largestX: largestXOfPreviousRootBlocks,
+        largestX: largestXOfPreviousRootBlocks
+      })
+
+      const repositionedBlocks = repositionBlocks(
+        blocks,
+        blocksArray,
+        startBlockKey,
+        activeBlocks,
+        largestXOfPreviousRootBlocks,
+        xOffset,
+        yOffset,
+        startBlockY,
+        startBlock.height,
+        startBlock.width
+      )
+
+      previousRootBlocks = repositionedBlocks
+
+      return {
+        ..._blocks,
+        ...repositionedBlocks
       }
-    )
-
-    const repositionedBlocks = repositionBlocks(
-      blocks,
-      blocksArray,
-      startBlockKey,
-      activeBlocks,
-      largestXOfPreviousRootBlocks,
-      xOffset,
-      yOffset,
-      startBlockY,
-      startBlock.height,
-      startBlock.width
-    )
-
-    previousRootBlocks = repositionedBlocks
-
-    return {
-      ..._blocks,
-      ...repositionedBlocks
-    }
-  }, {})
+    },
+    {}
+  )
 
   return _blocks
 }
