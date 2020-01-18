9db1336648c2ce326308d2fa35bd6b0a81fbae4e
richie-south <richard.soderman@gmail.com>
2020-01-18 11:23:29 +0100

    refactor: remove unnessesary

--- more-flow-app/src/lib/build-block.ts
+++ more-flow-app/src/lib/build-block.ts
@@ -1,59 +1,6 @@
 import { Blocks, Block } from "./block-types"
 import { getLargestXPosition, getSmallestXPosition, getCenterBlocksPosition } from "./get-positions"
 
-function buildSingleBlock (
-  blocks: Blocks,
-  blocksArray: Array<[string, Block]>,
-  parrentBlockKey: string,
-  child: [string, Block],
-  parrentBlock: Block,
-  {
-    yOffset = 150,
-    xOffset = 30,
-    largestX = 0,
-    blockPath = []
-  }: {
-    yOffset: number
-    xOffset: number
-    largestX: number
-    blockPath: Array<string>
-  }
-): Blocks {
-  const [childKey, childBlock] = child
-  const positionedChild = {
-    ...childBlock,
-    x: parrentBlock.x,
-    y: parrentBlock.y + yOffset
-  }
-
-  if (childBlock.parrents.length > 0) {
-    return {
-      ...buildBlock(
-        blocks,
-        blocksArray,
-        childKey,
-        {
-          x: positionedChild.x,
-          y: positionedChild.y,
-          height: positionedChild.height,
-          width: positionedChild.width,
-          largestX,
-          xOffset,
-          yOffset,
-          blockPath: [...blockPath, childKey]
-        }
-      ),
-      [parrentBlockKey]: parrentBlock,
-      [childKey]: positionedChild,
-    }
-  }
-
-  return {
-    [parrentBlockKey]: parrentBlock,
-    [childKey]: positionedChild
-  }
-}
-
 function buildFirstChildOfMany (
   blocks: Blocks,
   blocksArray: Array<[string, Block]>,
@@ -129,129 +76,104 @@ export function buildBlock (
       largestX = 0,
       blockPath = []
     }: BuildChildMeta
-  ): Blocks {
-    const parrentBlock = {
-      ...blocks[parrentBlockKey],
-      x,
-      y,
-      width,
-      height,
-    }
+): Blocks {
+  const parrentBlock = {
+    ...blocks[parrentBlockKey],
+    x,
+    y,
+    width,
+    height,
+  }
 
-    const children = blocksArray.filter(([_key, _block]) => {
-      return _block.parrents.includes(parrentBlockKey)
-    })
+  const children = blocksArray.filter(([_key, _block]) => {
+    return _block.parrents.includes(parrentBlockKey)
+  })
 
-    /**
-     * split children under parrent
-     */
-    if (children.length > 1) {
-      const positionedChildren = children.reduce((_children, [childKey, child], index) => {
-        if (index === 0) {
-          const firstChildTree = buildFirstChildOfMany(
-            blocks,
-            blocksArray,
-            children,
-            childKey,
-            {
-              parrentBlockX: parrentBlock.x,
-              parrentBlockY: parrentBlock.y,
-              parrentBlockWidth: parrentBlock.width,
-              childHeight: child.height,
-              childWidth: child.width,
-              blockPath,
-              largestX,
-              xOffset,
-              yOffset
-            }
-          )
-          return {
-            ..._children,
-            ...firstChildTree
-          }
+  /**
+   * split children under parrent
+   */
+  const positionedChildren = children.reduce((_children, [childKey, child], index) => {
+    if (index === 0) {
+      const firstChildTree = buildFirstChildOfMany(
+        blocks,
+        blocksArray,
+        children,
+        childKey,
+        {
+          parrentBlockX: parrentBlock.x,
+          parrentBlockY: parrentBlock.y,
+          parrentBlockWidth: parrentBlock.width,
+          childHeight: child.height,
+          childWidth: child.width,
+          blockPath,
+          largestX,
+          xOffset,
+          yOffset
         }
-
-        const _childrenArray = Object.entries(_children)
-        const largestXPosition = getLargestXPosition(_childrenArray)
-
-        const subTree = buildBlock(
-          blocks,
-          blocksArray,
-          childKey,
-          {
-            x: largestXPosition + (xOffset),
-            y: parrentBlock.y + yOffset,
-            height: child.height,
-            width: child.width,
-            yOffset,
-            xOffset,
-            largestX: largestXPosition,
-            blockPath: [...blockPath, childKey]
-          }
-        )
-
-        const _subTreeArray = Object.entries(subTree)
-        if (_subTreeArray.length > 1) {
-          const smallestX = getSmallestXPosition(_subTreeArray)
-          /**
-           * reposition children
-           * if coliding
-           */
-          if (smallestX < largestXPosition) {
-            return {
-              ..._children,
-              ...buildBlock(
-                blocks,
-                blocksArray,
-                childKey,
-                {
-                  // use larger offset when between two block trees
-                  x: largestXPosition + (largestXPosition - smallestX) + (xOffset * 3),
-                  y: parrentBlock.y + yOffset,
-                  height: child.height,
-                  width: child.width,
-                  yOffset,
-                  xOffset,
-                  largestX: largestXPosition + (largestXPosition - smallestX) + xOffset,
-                  blockPath: [...blockPath, childKey]
-                }
-              )
-            }
-          }
-        }
-
-        return {
-          ..._children,
-          ...subTree
-        }
-      }, {} as Blocks)
-
+      )
       return {
-        ...positionedChildren,
-        [parrentBlockKey]: parrentBlock
+        ..._children,
+        ...firstChildTree
       }
     }
 
-    /**
-     * single child should be directly under
-     */
-    if (children.length === 1) {
-      return buildSingleBlock(
-        blocks,
-        blocksArray,
-        parrentBlockKey,
-        children[0],
-        parrentBlock,
-        {
-          yOffset,
-          xOffset,
-          largestX,
-          blockPath
+    const _childrenArray = Object.entries(_children)
+    const largestXPosition = getLargestXPosition(_childrenArray)
+
+    const subTree = buildBlock(
+      blocks,
+      blocksArray,
+      childKey,
+      {
+        x: largestXPosition + (xOffset),
+        y: parrentBlock.y + yOffset,
+        height: child.height,
+        width: child.width,
+        yOffset,
+        xOffset,
+        largestX: largestXPosition,
+        blockPath: [...blockPath, childKey]
+      }
+    )
+
+    const _subTreeArray = Object.entries(subTree)
+    if (_subTreeArray.length > 1) {
+      const smallestX = getSmallestXPosition(_subTreeArray)
+      /**
+       * reposition children
+       * if coliding
+       */
+      if (smallestX < largestXPosition) {
+        return {
+          ..._children,
+          ...buildBlock(
+            blocks,
+            blocksArray,
+            childKey,
+            {
+              // use larger offset when between two block trees
+              x: largestXPosition + (largestXPosition - smallestX) + (xOffset * 3),
+              y: parrentBlock.y + yOffset,
+              height: child.height,
+              width: child.width,
+              yOffset,
+              xOffset,
+              largestX: largestXPosition + (largestXPosition - smallestX) + xOffset,
+              blockPath: [...blockPath, childKey]
+            }
+          )
         }
-      )
+      }
     }
 
     return {
-      [parrentBlockKey]: parrentBlock
+      ..._children,
+      ...subTree
     }
+  }, {} as Blocks)
+
+  return {
+    ...positionedChildren,
+    [parrentBlockKey]: parrentBlock
   }
+}
