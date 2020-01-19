19a0000f1c79767887a0a35296df5d1f2d37484c
richie-south <richard.soderman@gmail.com>
2020-01-19 19:39:10 +0100

    chore: change start blocks

--- more-flow-app/src/start-blocks.ts
+++ more-flow-app/src/start-blocks.ts
@@ -1,7 +1,7 @@
 import { Blocks } from "./lib/block-types";
 
 export const startBlocks: Blocks = {
-  /* 'new-visitor': {
+  'new-visitor': {
     type: 'new-visitor-block',
     typeMeta: {},
     data: {},
@@ -35,77 +35,6 @@ export const startBlocks: Blocks = {
     height: 120,
     width: 320,
   },
-
-   'support-match': {
-    type: 'match-block',
-    typeMeta: {
-      match: '/support'
-    },
-    data: {},
-    parrents: ['new-visitor', 'change-style'],
-    x: 0,
-    y: 0,
-    height: 120,
-    width: 320,
-  },
-
-  '4': {
-    type: 'standard-block',
-    typeMeta: {},
-    data: {},
-    parrents: ['support-match'],
-    x: 0,
-    y: 0,
-    height: 120,
-    width: 320,
-  },
-  '5': {
-    type: 'standard-block',
-    typeMeta: {},
-    data: {},
-    parrents: ['support-match'],
-    x: 0,
-    y: 0,
-    height: 120,
-    width: 320,
-  }, */
-
-
-
-  'new-visitor': {
-    type: 'new-visitor-block',
-    typeMeta: {},
-    data: {},
-    parrents: [],
-    x: 0,
-    y: 0,
-    height: 80,
-    width: 220,
-  },
-
-  /* 'is-device-mobile': {
-    type: 'match-block',
-    typeMeta: {
-      match: 'mobile'
-    },
-    data: {},
-    parrents: ['new-visitor'],
-    x: 0,
-    y: 0,
-    height: 120,
-    width: 320,
-  },
-
-  'change-style': {
-    type: 'standard-block',
-    typeMeta: {},
-    data: {},
-    parrents: ['is-device-mobile'],
-    x: 0,
-    y: 0,
-    height: 120,
-    width: 320,
-  }, */
 
   'support-match': {
     type: 'match-block',
@@ -171,7 +100,7 @@ export const startBlocks: Blocks = {
     x: 0,
     y: 0,
     height: 120,
-    width: 320,
+    width: 520,
   },
 
   '9': {
@@ -184,48 +113,4 @@ export const startBlocks: Blocks = {
     height: 120,
     width: 320,
   },
-
-
-
-  'checkout-match': {
-    type: 'match-block',
-    typeMeta: {
-      match: '/checkout'
-    },
-    data: {},
-    parrents: ['new-visitor'],
-    x: 0,
-    y: 0,
-    height: 120,
-    width: 320,
-  },
-
-
-  'product-page-match': {
-    type: 'match-block',
-    typeMeta: {
-      match: '/product/*'
-    },
-    data: {},
-    parrents: ['new-visitor'],
-    x: 0,
-    y: 0,
-    height: 160,
-    width: 520,
-  },
-
-  'start-page-match': {
-    type: 'match-block',
-    typeMeta: {
-      match: '/'
-    },
-    data: {},
-    parrents: ['new-visitor'],
-    x: 0,
-    y: 0,
-    height: 120,
-    width: 320,
-  },
-
-
 }
