a4ee924b96031ef211762417b578d2435901f3f3
richie-south <richard.soderman@gmail.com>
2020-01-19 11:47:26 +0100

    feat: test of drag support

--- more-flow-app/src/components/blocks/new-visitor-block.tsx
+++ more-flow-app/src/components/blocks/new-visitor-block.tsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import {
   StandardBlockContainer,
   StandardBlockProps,
-  TitleContainer,
   Title,
   IconContainer,
   TitleCenterContainer
 } from './standard-block'
 import eyeBlue from '../../assets/eye-blue.svg'
 
-export const NewVisitorBlock: React.FC<StandardBlockProps> = ({
+type NewVisitorBlockProps = {
+  handleDrag: (key: string, x: number, y: number) => void
+}
+
+export const NewVisitorBlock: React.FC<StandardBlockProps & NewVisitorBlockProps & any> = ({
   widthProp,
   heightProp,
   top,
@@ -17,7 +20,12 @@ export const NewVisitorBlock: React.FC<StandardBlockProps> = ({
   block,
   blockKey,
   children,
+  handleDrag
 }) => {
+  const lastPositions = useRef({
+    x: 0,
+    y: 0
+  }).current
 
   return (
     <StandardBlockContainer
@@ -25,6 +33,31 @@ export const NewVisitorBlock: React.FC<StandardBlockProps> = ({
       heightProp={heightProp}
       top={top}
       left={left}
+      draggable
+      onDragCapture={(event) => {
+        event.persist()
+        const { clientX, clientY } = event
+
+        if (clientX !== lastPositions.x || clientY !== lastPositions.y) {
+          lastPositions.x = clientX
+          lastPositions.y = clientY
+          handleDrag(
+            blockKey,
+            clientX,
+            clientY
+          )
+        }
+
+      }}
+      onDragEnd={(event) => {
+        event.persist()
+        const {clientX, clientY } = event
+        handleDrag(
+          blockKey,
+          clientX,
+          clientY
+        )
+      }}
     >
       <TitleCenterContainer>
         <IconContainer>
